f64325c00d40f54b4f32a335cfa87947cdafd8f1
Jan Palas <jan.palas@gmail.com>
2017-07-25 11:11:39 +0200

    Přidán NotesListPageViewModel

---
+++ NoteTaker/NoteTaker.Shared/ViewModels/Page/NotesListPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using NoteTaker.Shared.Models;
+
+namespace NoteTaker.Shared.ViewModels.Page
+{
+    public class NotesListPageViewModel
+    {
+        private readonly INotesManager _notesManager;
+
+        public ObservableCollection<Note> Notes { get; set; }
+
+        public NotesListPageViewModel(INotesManager notesManager)
+        {
+            _notesManager = notesManager;
+        }
+
+        public void LoadNotes()
+        {
+            Notes = new ObservableCollection<Note>(_notesManager.GetNotes());
+        }
+    }
+}
